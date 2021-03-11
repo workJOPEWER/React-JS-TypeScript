50ca36ec260e7f4af54c03c5104217bf5459dad6
workJopewer <arbuztatiana@gmail.com>
2021-03-11 18:23:38 +0200

    add todolist + interface and type props exmpl

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar} from './components/Navbar';
 import {TodoForm} from './components/TodoForm';
+import {TodoList} from './components/TodoList';
+import {ITodo} from "./interfaces";
 
 const App: React.FC = () => {
+    //<never> по умолчанию
+    const [todos, setTodos] = useState<ITodo[]>([])
+
+    const addHandler = (title: string) => {
+        const newTodo: ITodo = {
+            title: title,
+            id: Date.now(),
+            completed: false
+        }
+
+        const toogleHandler = (id: number) => {}
+
+        const removeHandler = (id: number) => {}
+
+        //перетираем стейт/нет гарантии что это будет предыдущее состояние
+        // setTodos([newTodo, ...todos])
+
+        setTodos(prev => [newTodo, ...prev])
+    }
+
     return <>
-    <Navbar />
+        <Navbar/>
         <div className={'container'}>
 
-            <TodoForm />
+            {/*react*/}
+            {/*<TodoForm onAdd={addHandler}/>*/}
+            <TodoForm onAdd={addHandler}/>
 
+            <TodoList todos={todos} />
         </div>
     </>
 
