798acfb4eb7aa244f16e6aa25654e00b4c849a1f
workJopewer <arbuztatiana@gmail.com>
2021-03-11 17:54:10 +0200

    second method with useRef

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,29 +1,39 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 
-export const TodoForm: React.FC = () => {
+//входящие свойства в компонент
+interface TodoFormProps {
+    onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
     //обрабатываем состояние input и изменяем локальный стейт
 
-    // const [title, setTitle] = useState('')
-    const [title, setTitle] = useState<string>('')
+    // // const [title, setTitle] = useState('')
+    // const [title, setTitle] = useState<string>('')
 
-    //обрабатываем событие
-    //event тип ChangeEvent-> тип HTMLInputElement
-    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value)
-    }
+    const ref = useRef<HTMLInputElement>(null)
+
+    // //обрабатываем событие
+    // //event тип ChangeEvent-> тип HTMLInputElement
+    // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(event.target.value)
+    // }
     //обрабатываем нажатие клавиши enter
     const keyPressHandler = (event: React.KeyboardEvent) => {
         if (event.key === 'Enter') {
-            console.log(title)
-            setTitle('')
+            props.onAdd(ref.current!.value) //all ok
+            ref.current!.value = ''
+            // console.log(title)
+            // setTitle('')
         }
     }
 
     return (
         <div className={'input-field mt2'}>
             <input
-                onChange={changeHandler}
-                value={title}
+                // onChange={changeHandler}
+                // value={title}
+                ref={ref}
                 type="text"
                 id="title"
                 placeholder={'Введите название дела'}
